b81df2e8bacefe45ad12c66a2218b81d9b3e03c1
Riccardo Enrico <riccardo.enrico97@gmail.com>
2025-07-18 14:16:38 +0000

    refactor(nodes): remove ButterflyPathNode and related files

--- src/uav_planning/uav_planning/nodes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ROS 2 Node Implementations
-
-This module contains the main ROS 2 node implementations for UAV planning.
-"""
-
-from .uav_planning_node import ButterflyPathNode
-
-__all__ = ["ButterflyPathNode"]
