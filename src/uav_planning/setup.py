7f15b7d9cbfc62404e7b794e12e335c570549fbe
Riccardo Enrico <riccardo.enrico97@gmail.com>
2025-07-18 14:23:07 +0000

    refactor(setup): remove unused launch file entries and clean up console scripts

--- src/uav_planning/setup.py
+++ src/uav_planning/setup.py
@@ -14,11 +14,6 @@ setup(
             ["resource/" + package_name],
         ),
         ("share/" + package_name, ["package.xml"]),
-        # Launch files organized in subfolders
-        (
-            os.path.join("share", package_name, "launch"),
-            ["launch/test_launch.sh"],  # Include shell scripts in root launch
-        ),
         (
             os.path.join("share", package_name, "launch", "demos"),
             glob("launch/demos/*.launch.py"),
@@ -41,19 +36,14 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            # Core components
             "uav_controller = uav_planning.core.uav_controller:main",
             "uav_monitor = uav_planning.core.uav_monitor:main",
             "uav_state_machine = uav_planning.core.uav_state_machine:main",
-            # Algorithm nodes
             "bioinspired_planner = uav_planning.algorithms.bioinspired_path_generator:main",
-            # Main planning nodes
             "butterfly_path_node = uav_planning.nodes.uav_planning_node:main",
-            # Action servers and clients
             "bioinspired_planner_action = uav_planning.actions.bioinspired_path_generator_action:main",
             "bioinspired_path_generator_client = uav_planning.actions.bioinspired_path_generator_client:main",
             "path_generator_client = uav_planning.actions.path_generator_client:main",
-            # Utilities
             "architecture_test = uav_planning.utils.architecture_test:main",
             "state_machine_demo = uav_planning.demos.state_machine_demo:main",
         ],
